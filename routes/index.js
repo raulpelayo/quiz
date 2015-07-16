b25cb19750072d2bb0f19a7caa1e0d6aa7e203e1
RAUL <raulpelayo@users.noreply.github.com>
2015-07-16 17:09:32 +0200

    Paso 8
    Añadido "acerca de"

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,7 @@ router.get('/', function(req, res) {
 /* rutas y manejo */
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+router.get('/author',function(req, res) {	res.render('author');});
+
 
 module.exports = router;
