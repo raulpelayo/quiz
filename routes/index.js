03befa64de260df983b0122e8f00a45861d5fd2a
RAUL <raulpelayo@users.noreply.github.com>
2015-08-03 17:11:37 +0200

    8.3
    Editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,8 @@ router.param('quizId', quizController.load);
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.get('/author',function(req, res) {	res.render('author');});
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',               quizController.create);
