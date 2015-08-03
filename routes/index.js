7ac673838be2a5687964c68b0d788c154f681833
RAUL <raulpelayo@users.noreply.github.com>
2015-08-03 17:19:08 +0200

    8.4
    Borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 router.get('/author',function(req, res) {	res.render('author');});
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',               quizController.create);
